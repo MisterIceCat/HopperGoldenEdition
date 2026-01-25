8d0fee703ff1457021c1bc10e60a93374694c2f2
onysd <beatvin128@gmail.com>
2026-01-25 23:41:51 +0200

    fixed crush on shift+click

--- src/main/java/mod/mistericecat/hoppergoldenedition/container/ContainerGoldenHopper.java
+++ src/main/java/mod/mistericecat/hoppergoldenedition/container/ContainerGoldenHopper.java
@@ -1,9 +1,11 @@
 package mod.mistericecat.hoppergoldenedition.container;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import net.minecraft.inventory.Container;
+import net.minecraft.item.ItemStack;
 import mod.mistericecat.hoppergoldenedition.tileentity.TileEntityGoldenHopper;
 
 public class ContainerGoldenHopper extends Container {
@@ -31,7 +33,58 @@ public class ContainerGoldenHopper extends Container {
     }
 
     @Override
-    public boolean canInteractWith(net.minecraft.entity.player.EntityPlayer playerIn) {
+    public boolean canInteractWith(EntityPlayer playerIn) {
         return hopperInv.isUsableByPlayer(playerIn);
+    }
+
+    @Override
+    public ItemStack transferStackInSlot(EntityPlayer playerIn, int index) {
+        ItemStack itemstack = ItemStack.EMPTY;
+        Slot slot = this.inventorySlots.get(index);
+
+        if (slot != null && slot.getHasStack()) {
+            ItemStack itemstack1 = slot.getStack();
+            itemstack = itemstack1.copy();
+
+            // Slot layout:
+            // 0: Filter slot (hopperInv slot 5)
+            // 1-5: Storage slots (hopperInv slots 0-4)
+            // 6-32: Player main inventory (27 slots: 3 rows × 9)
+            // 33-41: Player hotbar (9 slots)
+
+            if (index < 6) {
+                // Clicked slot is from hopper inventory (filter or storage)
+                // Transfer to player inventory
+                if (!this.mergeItemStack(itemstack1, 6, 42, true)) {
+                    return ItemStack.EMPTY;
+                }
+            } else {
+                // Clicked slot is from player inventory
+                // Try to transfer to hopper storage slots (slots 1-5) if item matches filter
+                if (hopperInv.isItemValidForSlot(0, itemstack1)) {
+                    // Try to merge into hopper storage slots first
+                    if (!this.mergeItemStack(itemstack1, 1, 6, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else {
+                    // Item doesn't match filter, cannot transfer to hopper
+                    return ItemStack.EMPTY;
+                }
+            }
+
+            if (itemstack1.isEmpty()) {
+                slot.putStack(ItemStack.EMPTY);
+            } else {
+                slot.onSlotChanged();
+            }
+
+            if (itemstack1.getCount() == itemstack.getCount()) {
+                return ItemStack.EMPTY;
+            }
+
+            slot.onTake(playerIn, itemstack1);
+        }
+
+        return itemstack;
     }
 }
