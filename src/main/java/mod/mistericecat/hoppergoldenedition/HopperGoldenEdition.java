b62bf96a54d8b822b292e821707c652b2f18ad08
MisterIceCat <mistericecat@gmail.com>
2026-04-25 23:20:14 +0200

    Fix mcmod.info

--- src/main/java/mod/mistericecat/hoppergoldenedition/HopperGoldenEdition.java
+++ src/main/java/mod/mistericecat/hoppergoldenedition/HopperGoldenEdition.java
@@ -15,9 +15,9 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = HopperGoldenEdition.MOD_ID, name = HopperGoldenEdition.NAME, version = HopperGoldenEdition.VERSION, acceptedMinecraftVersions = HopperGoldenEdition.ACCEPTED_VERSIONS)
 public class HopperGoldenEdition {
 
-    public static final String MOD_ID = Tags.MOD_ID;
-    public static final String NAME = Tags.NAME;
-    public static final String VERSION = Tags.VERSION;
+    public static final String MOD_ID = "hoppergoldenedition";
+    public static final String NAME = "Hopper Golden Edition";
+    public static final String VERSION = "1.0.3";
     public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 
     public static final Logger LOGGER = LogManager.getLogger(NAME);
